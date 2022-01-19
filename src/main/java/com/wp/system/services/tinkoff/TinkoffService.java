104083a9c80579868a9721bbc93da2e413e28254
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-01-19 23:30:13 +0300

    cors

--- src/main/java/com/wp/system/services/tinkoff/TinkoffService.java
+++ src/main/java/com/wp/system/services/tinkoff/TinkoffService.java
@@ -1,12 +1,35 @@
 package com.wp.system.services.tinkoff;
 
+import com.wp.system.entity.tinkoff.TinkoffIntegration;
+import com.wp.system.exception.ServiceException;
+import com.wp.system.repository.tinkoff.TinkoffIntegrationRepository;
 import com.wp.system.response.tinkoff.TinkoffAuthValidateResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 @Service
 public class TinkoffService {
 
+    @Autowired
+    private TinkoffIntegrationRepository tinkoffIntegrationRepository;
+
 //    public TinkoffAuthValidateResponse validateAuthRequest() {
 //
 //    }
+
+    public TinkoffIntegration getUserTinkoffIntegration(UUID id) {
+        return tinkoffIntegrationRepository.getTinkoffIntegrationByUserId(id).orElseThrow(() -> {
+            throw new ServiceException("Tinkoff integration not found", HttpStatus.NOT_FOUND);
+        });
+    }
+
+    public TinkoffIntegration getTinkoffIntegrationById(UUID id) {
+        return tinkoffIntegrationRepository.findById(id).orElseThrow(() -> {
+            throw new ServiceException("Tinkoff integration not found", HttpStatus.NOT_FOUND);
+        });
+    }
 }
