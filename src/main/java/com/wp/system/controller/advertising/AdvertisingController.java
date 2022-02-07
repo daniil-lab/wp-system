e3e638bfed2fe4a2689e93f91be4af78e11a4751
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-02-07 11:31:31 +0300

    fix

--- src/main/java/com/wp/system/controller/advertising/AdvertisingController.java
+++ src/main/java/com/wp/system/controller/advertising/AdvertisingController.java
@@ -4,6 +4,7 @@ import com.wp.system.dto.advertising.AdvertisingDTO;
 import com.wp.system.dto.advertising.AdvertisingViewDTO;
 import com.wp.system.dto.bill.BillDTO;
 import com.wp.system.dto.bill.BillTransactionDTO;
+import com.wp.system.exception.ServiceException;
 import com.wp.system.request.advertising.AddFileToAdvertisingRequest;
 import com.wp.system.request.advertising.CreateAdvertisingRequest;
 import com.wp.system.request.advertising.RemoveFileFromAdvertisingRequest;
@@ -18,13 +19,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import reactor.util.function.Tuple2;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -105,4 +110,25 @@ public class AdvertisingController {
                                                                                                @RequestParam Instant endDate) {
         return new ResponseEntity<>(new ServiceResponse<>(HttpStatus.OK.value(), this.advertisingService.getAdvertisingViewsByPeriod(advertisingId, startDate, endDate).stream().map(AdvertisingViewDTO::new).collect(Collectors.toList()), "Advertising returned"), HttpStatus.OK);
     }
+
+    @Operation(summary = "Получение контента рекламы")
+    @GetMapping(value = "/content/{contentName}")
+    public ResponseEntity<byte[]> getImageContent(
+            @PathVariable
+                    String contentName) {
+        try {
+            File image = new File("ads/" + contentName);
+
+            byte[] bytes = Files.readAllBytes(image.toPath());
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.valueOf(Files.probeContentType(image.toPath())));
+
+            return ResponseEntity.ok().headers(httpHeaders).body(bytes);
+        } catch (Exception e) {
+            throw new ServiceException("Error on send image", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
 }
