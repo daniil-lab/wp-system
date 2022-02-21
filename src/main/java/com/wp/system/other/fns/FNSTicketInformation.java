6b8336c461e4d08ecb0457c555685f7a4a525322
Daniil Duvanskiy <daniil@Mac-mini-Daniil.Dlink>
2022-02-21 16:47:47 +0300

    fix

--- src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
+++ src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
@@ -32,6 +32,26 @@ public class FNSTicketInformation extends FNSIntegration {
                 "\t</soap-env:Body>\n" +
                 "</soap-env:Envelope>", "POST", this.tempToken, "urn:SendMessageRequest");
 
+        System.out.println("<soap-env:Envelope xmlns:soap-env=\\\"http://schemas.xmlsoap.org/soap/envelope/\\\">\\n\" +\n" +
+                "                \"\\t<soap-env:Body>\\n\" +\n" +
+                "                \"\\t\\t<ns0:SendMessageRequest xmlns:ns0=\\\"urn://x-artefacts-gnivc-ru/inplat/servin/OpenApiAsyncMessageConsumerService/types/1.0\\\">\\n\" +\n" +
+                "                \"\\t\\t\\t<ns0:Message>\\n\" +\n" +
+                "                \"\\t\\t\\t\\t<tns:GetTicketRequest xmlns:tns=\\\"urn://x-artefacts-gnivc-ru/ais3/kkt/KktTicketService/types/1.0\\\">\\n\" +\n" +
+                "                \"\\t<tns:GetTicketInfo>\\n\" +\n" +
+                "                \"\\t<tns:Sum>\" + sum.toString() + \"</tns:Sum>\\n\" +\n" +
+                "                \"\\t\\t<tns:Date>\" + date + \"</tns:Date>\\n\" +\n" +
+                "                \"\\t\\t<tns:Fn>\" + fn + \"</tns:Fn>\\n\" +\n" +
+                "                \"\\t\\t<tns:TypeOperation>\" + operationType + \"</tns:TypeOperation>\\n\" +\n" +
+                "                \"\\t\\t<tns:FiscalDocumentId>\" + fiscalDocumentId + \"</tns:FiscalDocumentId>\\n\" +\n" +
+                "                \"\\t\\t<tns:FiscalSign>\" + fiscalSign + \"</tns:FiscalSign>\\n\" +\n" +
+                "                \"\\t\\t<tns:RawData>\" + rawData + \"</tns:RawData>\\n\" +\n" +
+                "                \"\\t\\t</tns:GetTicketInfo>\\n\" +\n" +
+                "                \"\\t</tns:GetTicketRequest>\\n\" +\n" +
+                "                \"\\t\\t\\t</ns0:Message>\\n\" +\n" +
+                "                \"\\t\\t</ns0:SendMessageRequest>\\n\" +\n" +
+                "                \"\\t</soap-env:Body>\\n\" +\n" +
+                "                \"</soap-env:Envelope>");
+
         return response;
     }
 }
