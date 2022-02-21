05406f2ba7b7f5536bc957f7580e35ae61a410f4
Daniil Duvanskiy <daniil@Mac-mini-Daniil.Dlink>
2022-02-21 16:48:23 +0300

    fix

--- src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
+++ src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
@@ -38,13 +38,13 @@ public class FNSTicketInformation extends FNSIntegration {
                 "                \"\\t\\t\\t<ns0:Message>\\n\" +\n" +
                 "                \"\\t\\t\\t\\t<tns:GetTicketRequest xmlns:tns=\\\"urn://x-artefacts-gnivc-ru/ais3/kkt/KktTicketService/types/1.0\\\">\\n\" +\n" +
                 "                \"\\t<tns:GetTicketInfo>\\n\" +\n" +
-                "                \"\\t<tns:Sum>\" + sum.toString() + \"</tns:Sum>\\n\" +\n" +
-                "                \"\\t\\t<tns:Date>\" + date + \"</tns:Date>\\n\" +\n" +
-                "                \"\\t\\t<tns:Fn>\" + fn + \"</tns:Fn>\\n\" +\n" +
-                "                \"\\t\\t<tns:TypeOperation>\" + operationType + \"</tns:TypeOperation>\\n\" +\n" +
-                "                \"\\t\\t<tns:FiscalDocumentId>\" + fiscalDocumentId + \"</tns:FiscalDocumentId>\\n\" +\n" +
-                "                \"\\t\\t<tns:FiscalSign>\" + fiscalSign + \"</tns:FiscalSign>\\n\" +\n" +
-                "                \"\\t\\t<tns:RawData>\" + rawData + \"</tns:RawData>\\n\" +\n" +
+                "                \"\\t<tns:Sum>" + sum.toString() + "</tns:Sum>\\n\" +\n" +
+                "                \"\\t\\t<tns:Date>" + date + "</tns:Date>\\n\" +\n" +
+                "                \"\\t\\t<tns:Fn>" + fn + "</tns:Fn>\\n\" +\n" +
+                "                \"\\t\\t<tns:TypeOperation>" + operationType + "</tns:TypeOperation>\\n\" +\n" +
+                "                \"\\t\\t<tns:FiscalDocumentId>" + fiscalDocumentId + "</tns:FiscalDocumentId>\\n\" +\n" +
+                "                \"\\t\\t<tns:FiscalSign>" + fiscalSign + "</tns:FiscalSign>\\n\" +\n" +
+                "                \"\\t\\t<tns:RawData>" + rawData + "</tns:RawData>\\n\" +\n" +
                 "                \"\\t\\t</tns:GetTicketInfo>\\n\" +\n" +
                 "                \"\\t</tns:GetTicketRequest>\\n\" +\n" +
                 "                \"\\t\\t\\t</ns0:Message>\\n\" +\n" +
