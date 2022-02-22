79b0800889a22271594db4674538a8472167d05b
Daniil Duvanskiy <daniil@Mac-mini-Daniil.Dlink>
2022-02-23 00:28:11 +0300

    fix

--- src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
+++ src/main/java/com/wp/system/other/fns/FNSTicketInformation.java
@@ -25,23 +25,23 @@ public class FNSTicketInformation extends FNSIntegration {
         }
 
         String response = FNSRequestSender.send("open-api/ais3/KktService/0.1", "<soap-env:Envelope xmlns:soap-env=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-                "   <soap-env:Body>\n" +
-                "      <ns0:SendMessageRequest xmlns:ns0=\"urn://x-artefacts-gnivc-ru/inplat/servin/OpenApiAsyncMessageConsumerService/types/1.0\">\n" +
-                "         <ns0:Message>\n" +
-                "            <tns:GetTicketRequest xmlns:tns=\"urn://x-artefacts-gnivc-ru/ais3/kkt/KktTicketService/types/1.0\">\n" +
-                "               <tns:GetTicketInfo>\n" +
-                "                  <tns:Sum>" + sum + "</tns:Sum>\n" +
-                "                  <tns:Date>" + date + "</tns:Date>\n" +
-                "                  <tns:Fn>" + fn + "</tns:Fn>\n" +
-                "                  <tns:TypeOperation>" + operationType + "</tns:TypeOperation>\n" +
-                "                  <tns:FiscalDocumentId>" + fiscalDocumentId + "</tns:FiscalDocumentId>\n" +
-                "                  <tns:FiscalSign>" + fiscalSign + "</tns:FiscalSign>\n" +
-                "               </tns:GetTicketInfo>\n" +
-                "            </tns:GetTicketRequest>\n" +
-                "         </ns0:Message>\n" +
-                "      </ns0:SendMessageRequest>\n" +
-                "   </soap-env:Body>\n" +
-                "</soap-env:Envelope>\n", "POST", this.tempToken, "urn:SendMessageRequest");
+                "<soap-env:Body>" +
+                "<ns0:SendMessageRequest xmlns:ns0=\"urn://x-artefacts-gnivc-ru/inplat/servin/OpenApiAsyncMessageConsumerService/types/1.0\">" +
+                "<ns0:Message>" +
+                "<tns:GetTicketRequest xmlns:tns=\"urn://x-artefacts-gnivc-ru/ais3/kkt/KktTicketService/types/1.0\">" +
+                "<tns:GetTicketInfo>" +
+                "<tns:Sum>" + sum + "</tns:Sum>" +
+                "<tns:Date>" + date + "</tns:Date>" +
+                "<tns:Fn>" + fn + "</tns:Fn>" +
+                "<tns:TypeOperation>" + operationType + "</tns:TypeOperation>" +
+                "<tns:FiscalDocumentId>" + fiscalDocumentId + "</tns:FiscalDocumentId>" +
+                "<tns:FiscalSign>" + fiscalSign + "</tns:FiscalSign>" +
+                "</tns:GetTicketInfo>" +
+                "</tns:GetTicketRequest>" +
+                "</ns0:Message>" +
+                "</ns0:SendMessageRequest>" +
+                "</soap-env:Body>" +
+                "</soap-env:Envelope>", "POST", this.tempToken, "urn:SendMessageRequest");
 
         return response;
     }
