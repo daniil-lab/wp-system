95bb8e9afef7123bc938d16ad2d9e707e0addafa
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-03-13 22:35:10 +0300

    fix

--- src/main/java/com/wp/system/permissions/PermissionManager.java
+++ src/main/java/com/wp/system/permissions/PermissionManager.java
@@ -17,6 +17,7 @@ import com.wp.system.permissions.loyalty.LoyaltyCardPermissions;
 import com.wp.system.permissions.notification.NotificationPermissions;
 import com.wp.system.permissions.subscription.SubscriptionPermissions;
 import com.wp.system.permissions.subscription.SubscriptionVariantPermissions;
+import com.wp.system.permissions.tinkoff.TinkoffPermissions;
 import com.wp.system.permissions.user.UserPermissions;
 import com.wp.system.permissions.user.UserRolePermissionPermissions;
 import com.wp.system.permissions.user.UserRolePermissions;
@@ -50,6 +51,7 @@ public class PermissionManager {
         permissionList.addAll(Arrays.stream(HelpLeadPermissions.values()).toList());
         permissionList.addAll(Arrays.stream(ActivityPermissions.values()).toList());
         permissionList.addAll(Arrays.stream(AdvertisingPermissions.values()).toList());
+        permissionList.addAll(Arrays.stream(TinkoffPermissions.values()).toList());
     }
 
     public Permission getPermissionBySystemName(String systemName) {
