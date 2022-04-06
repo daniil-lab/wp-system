d1f204c23ffb8234ba045542928782867d4bbf2c
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-04-06 15:31:47 +0300

    fix

--- src/main/java/com/wp/system/controller/DeeplinkController.java
+++ src/main/java/com/wp/system/controller/DeeplinkController.java
@@ -4,6 +4,7 @@ import com.wp.system.utils.DeeplinkObject;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Tag(name = "Deeplink")
 public class DeeplinkController {
-    @GetMapping("/apple-app-site-association")
+    @GetMapping(value = "/apple-app-site-association", produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DeeplinkObject> getDeeplinkObject() {
         return new ResponseEntity<>(new DeeplinkObject(), HttpStatus.OK);
     }
