f2aa4fb66f7f6924c60c414ba8726033b9003a55
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-04-06 16:13:15 +0300

    fix

--- src/main/java/com/wp/system/utils/DeeplinkObjectApplinksDetails.java
+++ src/main/java/com/wp/system/utils/DeeplinkObjectApplinksDetails.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class DeeplinkObjectApplinksDetails {
     private String appID = "3GBWH35VG2.walletbox.app";
 
-    private List<String> paths = List.of("/api/v1/tochka/auth-hook?code=*");
+    private List<String> paths = List.of("/api/v1/tochka/auth-hook");
 
     public DeeplinkObjectApplinksDetails() {
     }
