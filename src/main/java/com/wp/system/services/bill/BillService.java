6fa956ceec507c952f66de22fc860211420a9ba6
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-04-30 22:03:06 +0300

    fix

--- src/main/java/com/wp/system/services/bill/BillService.java
+++ src/main/java/com/wp/system/services/bill/BillService.java
@@ -133,7 +133,9 @@ public class BillService {
 
         BillTransaction transaction = facade.withdraw(request.getAmount(), request.getCents(), request.getDescription(), request.getTime());
 
+
         if(category != null && category.getCategoryLimit() != 0) {
+            category.setCategorySpend(category.getCategorySpend() + request.getAmount());
             category.setPercentsFromLimit((category.getCategorySpend() / category.getCategoryLimit()) * 100);
             categoryRepository.save(category);
         }
