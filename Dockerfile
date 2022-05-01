193b7a5d2393a9361bdf4306ceddf06f9a9a8139
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-05-01 16:02:17 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -14,9 +14,16 @@ FROM ghcr.io/graalvm/jdk:java17-21.3.0
 
 COPY --from=build /home/gradle/wp/build/libs/system-dev.jar .
 
+ ENV JAVA_TOOL_OPTIONS "-Dcom.sun.management.jmxremote.ssl=false \
+ -Dcom.sun.management.jmxremote.authenticate=false \
+ -Dcom.sun.management.jmxremote.port=5000 \
+ -Dcom.sun.management.jmxremote.rmi.port=5000 \
+ -Dcom.sun.management.jmxremote.host=0.0.0.0 \
+ -Djava.rmi.server.hostname=0.0.0.0"/
+
 COPY ./images ./images
 
-EXPOSE 8080
+EXPOSE [8080, 5000, 9696]
 
 ENTRYPOINT java -Xmx12144M -jar system-dev.jar
 
--- src/main/java/com/wp/system/config/ConfigureRMI.java
+++ src/main/java/com/wp/system/config/ConfigureRMI.java
@@ -10,25 +10,25 @@ import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 @Configuration
 public class ConfigureRMI {
 
-    private String rmiHost = "localhost";
-
-    private Integer rmiPort = 9696;
-
-    @Bean
-    public RmiRegistryFactoryBean rmiRegistry() {
-        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
-        rmiRegistryFactoryBean.setPort(rmiPort);
-        rmiRegistryFactoryBean.setAlwaysCreate(true);
-        return rmiRegistryFactoryBean;
-    }
-
-    @Bean
-    @DependsOn("rmiRegistry")
-    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
-        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
-        connectorServerFactoryBean.setObjectName("connector:name=rmi");
-        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
-        System.out.println(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
-        return connectorServerFactoryBean;
-    }
+//    private String rmiHost = "localhost";
+//
+//    private Integer rmiPort = 9696;
+//
+//    @Bean
+//    public RmiRegistryFactoryBean rmiRegistry() {
+//        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
+//        rmiRegistryFactoryBean.setPort(rmiPort);
+//        rmiRegistryFactoryBean.setAlwaysCreate(true);
+//        return rmiRegistryFactoryBean;
+//    }
+//
+//    @Bean
+//    @DependsOn("rmiRegistry")
+//    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+//        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
+//        connectorServerFactoryBean.setObjectName("connector:name=rmi");
+//        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+//        System.out.println(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+//        return connectorServerFactoryBean;
+//    }
 }
