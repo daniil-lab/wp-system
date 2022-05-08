71d1a14b55b01b173052ee6a993515deb9062863
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-05-08 11:48:21 +0300

    fix

--- src/main/java/com/wp/system/services/abstarct/AbstractService.java
+++ src/main/java/com/wp/system/services/abstarct/AbstractService.java
@@ -56,23 +56,23 @@ public class AbstractService {
         Query query = entityManager.createNativeQuery("""
                 ((select CAST(id as varchar), CAST(category_id as varchar), sum, action, create_at, currency, description, 'SYSTEM' as transaction_type, (SELECT b.name FROM bill as b WHERE b.id = bill_transaction.bill_id) as billName from bill_transaction WHERE create_at BETWEEN :startDate AND :endDate AND user_id = :userId)
                 union
-                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'TINKOFF' as transactionType, (SELECT tc.name FROM tinkoff_card as tc WHERE tc.id = tinkoff_transaction.card_id) as billName from tinkoff_transaction WHERE date BETWEEN :startDate AND :endDate AND (SELECT tinkc.integration_id FROM tinkoff_card as tinkc = tinkc.integration_id = :tinkoffIntegrationId))
+                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'TINKOFF' as transactionType, (SELECT tc.name FROM tinkoff_card as tc WHERE tc.id = tinkoff_transaction.card_id) as billName from tinkoff_transaction WHERE date BETWEEN :startDate AND :endDate AND SELECT  card_id in (select tc.id from tinkoff_card as tc where tc.integration_id = (select ti.id from tinkoff_integration as ti where ti.user_id = :userId)))
                 union
-                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'SBER' as transactionType, (SELECT sc.name FROM sber_card as sc WHERE sc.id = sber_transaction.card_id) as billName from sber_transaction WHERE date BETWEEN :startDate AND :endDate AND (SELECT sberc.integration_id FROM sber_card as sberc = sberc.integration_id = :sberIntegrationId))
-                union
-                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'TOCHKA' as transactionType, (SELECT tkc.card_number FROM tochka_card as tkc WHERE tkc.id = tochka_transaction.card_id) as billName from tochka_transaction WHERE date BETWEEN :startDate AND :endDate AND (SELECT tochkc.integration_id FROM tochka_card as tochkc = tochkc.integration_id = :tochkaIntegrationId))
-                ) order by create_at desc limit :limit offset :offset""");
+                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'SBER' as transactionType, (SELECT sc.name FROM sber_card as sc WHERE sc.id = sber_transaction.card_id) as billName from sber_transaction WHERE date BETWEEN :startDate AND :endDate AND card_id in (select sc.id from sber_card as sc where sc.integration_id = (select si.id from sber_integration as si where si.user_id = :userId)))
+                 order by create_at desc limit :limit offset :offset""");
+
+        // TOCHKA = union
+        //                (select CAST(id as varchar), CAST(category_id as varchar), CAST(CONCAT(CAST(amount as varchar),'.' ,CAST(cents as varchar)) as float) as sum, transaction_type, date, currency, description, 'TOCHKA' as transactionType, (SELECT tkc.card_number FROM tochka_card as tkc WHERE tkc.id = tochka_transaction.card_id) as billName from tochka_transaction WHERE date BETWEEN :startDate AND :endDate AND card_id in (select sc.id from sber_card as sc where sc.integration_id = (select si.id from sber_integration as si where si.user_id = :userId)))
+        //                )
 
         query.setParameter("userId", user.getId());
         query.setParameter("startDate", Timestamp.from(startDate));
         query.setParameter("endDate", Timestamp.from(endDate));
         query.setParameter("limit", pageSize);
         query.setParameter("offset", page * pageSize);
-        query.setParameter("tinkoffIntegrationId", tinkoffIntegration.isPresent() ? tinkoffIntegration.get().getId() : "no");
-        query.setParameter("sberIntegrationId", sberIntegration.isPresent() ? tinkoffIntegration.get().getId() : "no");
-        query.setParameter("tochkaIntegrationId", tochkaIntegration.isPresent() ? tinkoffIntegration.get().getId() : "no");
 
         List<Object[]> results = query.getResultList();
+
 
         List<AbstractTransactionDTO> transactions = new ArrayList<>();
         results.forEach(item -> {
