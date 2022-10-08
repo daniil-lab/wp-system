63af5e43e598ef40c09739e8cd4ed012b4ff91ab
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-10-08 20:03:45 +0300

    fix

--- src/main/java/com/wp/system/utils/sber/SberRegister.java
+++ src/main/java/com/wp/system/utils/sber/SberRegister.java
@@ -47,54 +47,58 @@ public class SberRegister {
     }
 
     public void register() {
-        if(!state.equals(SberRegisterState.WAIT_REGISTER))
-            throw new ServiceException("Try recreate sber register instance", HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            if(!state.equals(SberRegisterState.WAIT_REGISTER))
+                throw new ServiceException("Try recreate sber register instance", HttpStatus.INTERNAL_SERVER_ERROR);
 
-        state = SberRegisterState.REGISTER;
+            state = SberRegisterState.REGISTER;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-        map.add("operation", "register");
-        map.add("login", phone);
-        map.add("appType", "android");
-        map.add("version", "9.20");
-        map.add("appVersion", "10.2.0");
-        map.add("deviceName", "HUAWEI_ANE-LX1");
-        map.add("devID", "607d725604d1f032e50bb3c0622e791d3f400000");
-        map.add("devIdOld", "607d725604d1f032e50bb3c0622e791d3f400000");
+            MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+            map.add("operation", "register");
+            map.add("login", phone);
+            map.add("appType", "android");
+            map.add("version", "9.20");
+            map.add("appVersion", "10.2.0");
+            map.add("deviceName", "HUAWEI_ANE-LX1");
+            map.add("devID", "607d725604d1f032e50bb3c0622e791d3f400000");
+            map.add("devIdOld", "607d725604d1f032e50bb3c0622e791d3f400000");
 
-        map.add("mobileSdkData", "{\\\"TIMESTAMP\\\":\\\"2019-09-13T07:23:14Z\\\",\\\"HardwareID\\\":\\\"-1\\\",\\\"SIM_ID\\\":\\\"-1\\\",\\\"PhoneNumber\\\":\\\"-1\\\",\\\"GeoLocationInfo\\\":[{\\\"Timestamp\\\":\\\"0\\\",\\\"Status\\\":\\\"1\\\"}],\\\"DeviceModel\\\":\\\"ANE-LX1\\\",\\\"MultitaskingSupported\\\":true,\\\"DeviceName\\\":\\\"marky\\\",\\\"DeviceSystemName\\\":\\\"Android\\\",\\\"DeviceSystemVersion\\\":\\\"28\\\",\\\"Languages\\\":\\\"ru\\\",\\\"WiFiMacAddress\\\":\\\"02:00:00:00:00:00\\\",\\\"WiFiNetworksData\\\":{\\\"BBSID\\\":\\\"02:00:00:00:00:00\\\",\\\"SignalStrength\\\":\\\"-47\\\",\\\"Channel\\\":\\\"null\\\"},\\\"CellTowerId\\\":\\\"-1\\\",\\\"LocationAreaCode\\\":\\\"-1\\\",\\\"ScreenSize\\\":\\\"1080x2060\\\",\\\"RSA_ApplicationKey\\\":\\\"2C501591EA5BF79F1C0ABA8B628C2571\\\",\\\"MCC\\\":\\\"286\\\",\\\"MNC\\\":\\\"02\\\",\\\"OS_ID\\\":\\\"1f32651b72df5515\\\",\\\"SDK_VERSION\\\":\\\"3.10.0\\\",\\\"Compromised\\\":0,\\\"Emulator\\\":0}");
-        map.add("mobileSDKKAV", "{\\\"osVersion\\\":0,\\\"KavSdkId\\\":\\\"\\\",\\\"KavSdkVersion\\\":\\\"\\\",\\\"KavSdkVirusDBVersion\\\":\\\"SdkVirusDbInfo(year=0, month=0, day=0, hour=0, minute=0, second=0, knownThreatsCount=0, records=0, size=0)\\\",\\\"KavSdkVirusDBStatus\\\":\\\"\\\",\\\"KavSdkVirusDBStatusDate\\\":\\\"\\\",\\\"KavSdkRoot\\\":false,\\\"LowPasswordQuality\\\":false,\\\"NonMarketAppsAllowed\\\":false,\\\"UsbDebugOn\\\":false,\\\"ScanStatus\\\":\\\"NONE\\\"}");
+            map.add("mobileSdkData", "{\\\"TIMESTAMP\\\":\\\"2019-09-13T07:23:14Z\\\",\\\"HardwareID\\\":\\\"-1\\\",\\\"SIM_ID\\\":\\\"-1\\\",\\\"PhoneNumber\\\":\\\"-1\\\",\\\"GeoLocationInfo\\\":[{\\\"Timestamp\\\":\\\"0\\\",\\\"Status\\\":\\\"1\\\"}],\\\"DeviceModel\\\":\\\"ANE-LX1\\\",\\\"MultitaskingSupported\\\":true,\\\"DeviceName\\\":\\\"marky\\\",\\\"DeviceSystemName\\\":\\\"Android\\\",\\\"DeviceSystemVersion\\\":\\\"28\\\",\\\"Languages\\\":\\\"ru\\\",\\\"WiFiMacAddress\\\":\\\"02:00:00:00:00:00\\\",\\\"WiFiNetworksData\\\":{\\\"BBSID\\\":\\\"02:00:00:00:00:00\\\",\\\"SignalStrength\\\":\\\"-47\\\",\\\"Channel\\\":\\\"null\\\"},\\\"CellTowerId\\\":\\\"-1\\\",\\\"LocationAreaCode\\\":\\\"-1\\\",\\\"ScreenSize\\\":\\\"1080x2060\\\",\\\"RSA_ApplicationKey\\\":\\\"2C501591EA5BF79F1C0ABA8B628C2571\\\",\\\"MCC\\\":\\\"286\\\",\\\"MNC\\\":\\\"02\\\",\\\"OS_ID\\\":\\\"1f32651b72df5515\\\",\\\"SDK_VERSION\\\":\\\"3.10.0\\\",\\\"Compromised\\\":0,\\\"Emulator\\\":0}");
+            map.add("mobileSDKKAV", "{\\\"osVersion\\\":0,\\\"KavSdkId\\\":\\\"\\\",\\\"KavSdkVersion\\\":\\\"\\\",\\\"KavSdkVirusDBVersion\\\":\\\"SdkVirusDbInfo(year=0, month=0, day=0, hour=0, minute=0, second=0, knownThreatsCount=0, records=0, size=0)\\\",\\\"KavSdkVirusDBStatus\\\":\\\"\\\",\\\"KavSdkVirusDBStatusDate\\\":\\\"\\\",\\\"KavSdkRoot\\\":false,\\\"LowPasswordQuality\\\":false,\\\"NonMarketAppsAllowed\\\":false,\\\"UsbDebugOn\\\":false,\\\"ScanStatus\\\":\\\"NONE\\\"}");
 
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
-        ResponseEntity<String> response = restTemplate.postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", request , String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity( "https://online.sberbank.ru:4477/CSAMAPI/registerApp.do", request , String.class);
 
-        System.out.println(response.getBody());
+            System.out.println(response.getBody());
 
-        Integer responseCode = SberUtils.getCodeFromResponse(response.getBody());
+            Integer responseCode = SberUtils.getCodeFromResponse(response.getBody());
 
-        if(responseCode == null || responseCode != 0) {
-            throw new ServiceException("Invalid response code from SBER", HttpStatus.INTERNAL_SERVER_ERROR);
+            if(responseCode == null || responseCode != 0) {
+                throw new ServiceException("Invalid response code from SBER", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            String responseMGUID = SberUtils.getMGUIDFromResponse(response.getBody());
+
+            if(responseMGUID == null)
+                throw new ServiceException("Invalid MGUID", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            mGUID = responseMGUID;
+
+            String sessionCookie = SberUtils.exportSessionCookieFromCookies(response.getHeaders());
+
+            if(sessionCookie == null)
+                throw new ServiceException("Can`t exact session", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            jSession = sessionCookie;
+
+            state = SberRegisterState.WAIT_SUBMIT;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        String responseMGUID = SberUtils.getMGUIDFromResponse(response.getBody());
-
-        if(responseMGUID == null)
-            throw new ServiceException("Invalid MGUID", HttpStatus.INTERNAL_SERVER_ERROR);
-
-        mGUID = responseMGUID;
-
-        String sessionCookie = SberUtils.exportSessionCookieFromCookies(response.getHeaders());
-
-        if(sessionCookie == null)
-            throw new ServiceException("Can`t exact session", HttpStatus.INTERNAL_SERVER_ERROR);
-
-        jSession = sessionCookie;
-
-        state = SberRegisterState.WAIT_SUBMIT;
     }
 
     public void submitRegister(String code) {
