b48acd140f5f8f48398accf707313e93bd705107
Daniil Duvanskiy <daniil@Mac-mini-Daniil.local>
2022-10-09 02:46:43 +0300

    fix

--- src/main/java/com/wp/system/services/abstarct/AbstractService.java
+++ src/main/java/com/wp/system/services/abstarct/AbstractService.java
@@ -133,8 +133,8 @@ public class AbstractService {
 
         // TOCHKA =                 union
         //                (select CAST(id as varchar), CAST(category_id as varchar), amount as sum, transaction_type, date, currency, description, 'TOCHKA' as transactionType, (SELECT tkc.card_number FROM tochka_card as tkc WHERE tkc.id = tochka_transaction.card_id) as billName from tochka_transaction WHERE date BETWEEN :startDate AND :endDate AND card_id in (select sc.id from sber_card as sc where sc.integration_id = (select si.id from sber_integration as si where si.user_id = :userId)))
-
-        query.setParameter("userId", user.getId());
+        if(query.toString().contains("userId"))
+            query.setParameter("userId", user.getId());
 
         if(billId != null)
             query.setParameter("cardId", user.getId());
